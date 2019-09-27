c1929418ec98bb5e74ff8bc067ef54fca3c008e5
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-27 19:56:53 +0100

    setup featured products section

--- src/components/HomePage/Featured.js
+++ src/components/HomePage/Featured.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Product from '../Product';
+import Title from '../Title';
+import { ProductConsumer } from '../../context';
 
 const Featured = () => {
-  return <div>hello fro featured</div>;
+  return (
+    <section className='py-5'>
+      <div className='container'>
+        {/* title */}
+        <Title title='featured products' center='true' />
+        {/* products */}
+        <div className='row'>
+          <ProductConsumer>
+            {value => {
+              const { featuredProducts } = value;
+              return featuredProducts.map(product => (
+                <Product key={product.id} product={product}></Product>
+              ));
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Featured;
---
+++ src/components/Product.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Product = ({ product }) => {
+  return (
+    <div>
+      <h2>product component</h2>
+    </div>
+  );
+};
+
+export default Product;
