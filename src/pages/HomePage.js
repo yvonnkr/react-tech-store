428f95226070f5e5fee110e5f5f1ae70ba5f80ca
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-27 12:26:01 +0100

    Setup main-link class css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,7 +6,9 @@ const HomePage = () => {
   return (
     <>
       <Hero title='awesome gadgets' max='true'>
-        <Link to='/products'>our products</Link>
+        <Link to='/products' className='main-link' style={{ margin: '2rem' }}>
+          our products
+        </Link>
       </Hero>
     </>
   );
