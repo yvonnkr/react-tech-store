8ab1a64c64cd9ab579a422a4b35ddb81e2f828a0
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-27 12:39:03 +0100

    Setup default/404 page

--- src/pages/DefaultPage.js
+++ src/pages/DefaultPage.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import defaultBcg from '../images/defaultBcg.jpeg';
+import { Link } from 'react-router-dom';
 
 const DefaultPage = () => {
   return (
     <>
-      <h1>Hello from DefaultPage</h1>
+      <Hero img={defaultBcg} title='404' max='true'>
+        <h2 className='text-uppercase'>page not found</h2>
+        <Link to='/' className='main-link' style={{ marginTop: '2rem' }}>
+          return home
+        </Link>
+      </Hero>
     </>
   );
 };
