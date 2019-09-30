58d5b7d59e26a49c525220cbe94c1521446e8f85
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-30 16:37:34 +0100

    setup CartTotal Component

--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { ProductConsumer } from '../../context/context';
 
 const CartTotals = () => {
-  return <div>CartTotals</div>;
+  return (
+    <div className='container'>
+      <div className='row'>
+        <ProductConsumer>
+          {value => {
+            const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+            return (
+              <div className='col text-title text-center my-4'>
+                <button
+                  className='btn btn-outline-danger text-capitalize mb-4'
+                  onClick={clearCart}
+                >
+                  clear cart
+                </button>
+                <h3>subtotal : £{cartSubTotal}</h3>
+                <h3>tax : £{cartTax}</h3>
+                <h3>total : £{cartTotal}</h3>
+              </div>
+            );
+          }}
+        </ProductConsumer>
+      </div>
+    </div>
+  );
 };
 
 export default CartTotals;
