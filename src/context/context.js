c240accc67a043ce7b596b3ec7f6d75b52b4a17d
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-30 13:06:43 +0100

    setSingleProduct and getStorageProduct methods

--- src/context/context.js
+++ src/context/context.js
@@ -20,7 +20,7 @@ class ProductProvider extends Component {
     filteredProducts: [],
     featuredProducts: [],
     singleProduct: {},
-    loading: false
+    loading: true
   };
 
   componentDidMount() {
@@ -68,7 +68,9 @@ class ProductProvider extends Component {
 
   //get product from local storage
   getStorageProduct = () => {
-    return {};
+    return localStorage.getItem('singleProduct')
+      ? JSON.parse(localStorage.getItem('singleProduct'))
+      : {};
   };
 
   //get totals
@@ -142,7 +144,13 @@ class ProductProvider extends Component {
 
   //set single product
   setSingleProduct = id => {
-    console.log(`set single product ${id}`);
+    let product = this.state.storeProducts.find(item => item.id === id);
+    localStorage.setItem('singleProduct', JSON.stringify(product));
+
+    this.setState({
+      singleProduct: { ...product },
+      loading: false
+    });
   };
 
   // handle sidebar
