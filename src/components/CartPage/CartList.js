98d826db2cb5d2e1c23e0e6f6d24cce6e14053fb
Yvonne Nkr <yvonnkr86@gmail.com>
2019-09-30 16:58:28 +0100

    setup CartList Component

--- src/components/CartPage/CartList.js
+++ src/components/CartPage/CartList.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import CartItem from './CartItem';
+import { ProductConsumer } from '../../context/context';
 
 const CartList = () => {
   return (
-    <div>
-      CartList
-      <CartItem />
+    <div className='container-fluid'>
+      <div className='row'>
+        <div className='col'>
+          <ProductConsumer>
+            {value => {
+              const { cart, increment, decrement, removeItem } = value;
+              if (cart.length === 0) {
+                return (
+                  <h1 className='text-title text-center my-4'>
+                    your cart is currently empty
+                  </h1>
+                );
+              }
+              return (
+                <>
+                  {cart.map(item => (
+                    <CartItem
+                      key={item.id}
+                      cartItem={item}
+                      increment={increment}
+                      decrement={decrement}
+                      removeItem={removeItem}
+                    />
+                  ))}
+                </>
+              );
+            }}
+          </ProductConsumer>
+        </div>
+      </div>
     </div>
   );
 };
